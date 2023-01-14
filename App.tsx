f0ad0186014431a86a56752be96e5f6d3735888a
Victor <vtrownz@hotmail.com>
2023-01-14 03:28:05 -0300

    expo upgrade

--- App.tsx
+++ App.tsx
@@ -18,8 +18,8 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-  },
+    paddingTop: Constants.statusBarHeight
+  }
 });
 
 export default App;
